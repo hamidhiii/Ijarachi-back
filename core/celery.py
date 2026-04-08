37e88c4667501ba755291c0a027e90798d3a700f
bositxonn <londonpreff@gmail.com>
2026-04-08 09:01:27 +0500

    chore: align docker network mappings and fix celery beat scheduler

--- core/celery.py
+++ core/celery.py
@@ -6,3 +6,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 app = Celery('synth_share')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+from celery.schedules import crontab
+
+app.conf.beat_schedule = {
+    'notify_expiring_bookings_daily': {
+        'task': 'apps.bookings.tasks.notify_expiring_bookings',
+        'schedule': crontab(hour=10, minute=0),  # Everyday at 10:00 
+    },
+    'auto_complete_inspections_hourly': {
+        'task': 'apps.bookings.tasks.auto_complete_inspections',
+        'schedule': crontab(minute=0),  # Every top of the hour
+    },
+}
